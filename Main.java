95fc297a15d00ae7268f93cfb44925829359c44b
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2021-12-02 15:37:52 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 
 import java.awt.BorderLayout;
-import java.awt.LayoutManager;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
@@ -9,19 +11,46 @@ public class Main {
     public static void main(String args[]){
        JFrame frame = new JFrame("Food-ease");
        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       frame.setSize(600,600);
+       frame.setSize(500,350);
+       
+
        JButton clockIn = new JButton("Clock In/Clock Out");
-       
        JButton tables= new JButton("Table View");
-   
-       
        JButton orders = new JButton("Orders");
-     
-       frame.add(clockIn, BorderLayout.NORTH);
-       frame.add(tables, BorderLayout.SOUTH);
-       frame.add(orders, BorderLayout.CENTER);
+       JButton kitchen= new JButton("Kitchen");
+       JButton inventory = new JButton("Inventory");
+
+
+
+       JPanel content = new JPanel();
+       content.setLayout(new BorderLayout());
+        content.setBackground(Color.GRAY);
+       content.add(clockIn, BorderLayout.NORTH);
+       content.add(tables, BorderLayout.CENTER);
+       content.add(orders, BorderLayout.WEST);
+       content.add(kitchen, BorderLayout.EAST);
+       content.add(inventory, BorderLayout.SOUTH);
+       
+       frame.add(content);
+        frame.setLocation(200,200);
        frame.setVisible(true);
+
+       clockIn.addActionListener(new ActionListener(){
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JPanel clock = new JPanel();
+            frame.setContentPane(clock);
+            frame.invalidate();
+            frame.validate();
+
+            
+        }
+
+       
+    });
 
 
     }
 }
+

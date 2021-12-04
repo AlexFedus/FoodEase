b67a3d90c2768b85c9d49eb63531cec43b00b7cb
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2021-12-04 15:11:15 -0500

    Updated Clock and Tables pages

--- src/Clock.java
+++ src/Clock.java
@@ -1,13 +1,25 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class Clock extends JPanel {
 
     public static JPanel newJPanel() {
         JPanel clock = new JPanel();
+        clock.setLayout(new FlowLayout());
         JButton button = new JButton("Home");
+        JLabel employeeID = new JLabel("Emoloyee ID");
+        JTextField id = new JTextField(20);
+        JButton clockIn = new JButton("Clock In");
+        JButton clockOut = new JButton("Clock Out");
+
+        
         clock.add(button);
+        clock.add(employeeID);
+        clock.add(id);
+        clock.add(clockIn);
+        clock.add(clockOut);
 
         button.addActionListener(new ActionListener(){
 
@@ -21,6 +33,8 @@ public class Clock extends JPanel {
             }
             
         });
+
+        
 
         return clock;
     }
--- src/Tables.java
+++ src/Tables.java
@@ -1,13 +1,74 @@
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.*;
 
 public class Tables extends JPanel {
 
     public static JPanel newJPanel() {
         JPanel tables = new JPanel();
+        tables.setLayout(new FlowLayout());
         JButton button = new JButton("Home");
+        JButton table1 = new JButton("1");
+        JButton table2 = new JButton("2");
+        JButton table3 = new JButton("3");
+        JButton table4 = new JButton("4");
+        JButton table5 = new JButton("5");
+        JButton table6 = new JButton("6");
+        JButton table7 = new JButton("7");
+        JButton table8 = new JButton("8");
+        JButton table9 = new JButton("9");
+        JButton table10 = new JButton("10");
+        JButton table11 = new JButton("11");
+        JButton table12 = new JButton("12");
+        JButton table13 = new JButton("13");
+        JButton table14 = new JButton("14");
+        JButton table15 = new JButton("15");
+        JButton table16 = new JButton("16");
+        JButton table17 = new JButton("17");
+        JButton table18 = new JButton("18");
+        JButton table19 = new JButton("19");
+        JButton table20 = new JButton("20");
+        JButton table21 = new JButton("21");
+        JButton table22 = new JButton("22");
+        JButton table23 = new JButton("23");
+        JButton table24 = new JButton("24");
+        JButton table25 = new JButton("25");
+
         tables.add(button);
+
+        tables.add(table1);
+        tables.add(table2);
+        tables.add(table3);
+        tables.add(table4);
+        tables.add(table5);
+        tables.add(table6);
+        tables.add(table7);
+        tables.add(table8);
+        tables.add(table9);
+        tables.add(table10);
+        tables.add(table11);
+        tables.add(table12);
+        tables.add(table13);
+        tables.add(table14);
+        tables.add(table15);
+        tables.add(table16);
+        tables.add(table17);
+        tables.add(table18);
+        tables.add(table19);
+        tables.add(table20);
+        tables.add(table21);
+        tables.add(table22);
+        tables.add(table23);
+        tables.add(table24);
+        tables.add(table25);
+
+        
+    
+
+
+
+        
 
         button.addActionListener(new ActionListener(){
 
@@ -21,6 +82,13 @@ public class Tables extends JPanel {
             }
             
         });
+
+        
+            
+
+        
+
+
 
         return tables;
     }

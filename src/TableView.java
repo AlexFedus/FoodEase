d16e480835aa6ff6d83481d90cf06ee5cf632360
AlexFedus <83235408+AlexFedus@users.noreply.github.com>
2021-12-05 12:02:18 -0500

    Added availability status to tables
    Bug: Changing one status changes every tables status

--- src/TableView.java
+++ src/TableView.java
@@ -2,18 +2,45 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+
+
 
 
 public class TableView extends JPanel {
+    static String status = "";
 
     public static JPanel newJPanel() {
         JPanel tables = new JPanel();
-        tables.setLayout(new GridLayout(4,2));
+        tables.setLayout(new GridLayout(5,2));
         JButton button = new JButton("Back");
+        JLabel availability = new JLabel("Set Availability:", SwingConstants.CENTER);
+        availability.setFont(new Font("Serif", Font.PLAIN, 20));
+
+        JButton submit = new JButton("Submit");
+        
+
+        String[] choices = {
+            "Available", "Occupied"
+        };
+        
+        JComboBox choose = new JComboBox(choices);
+
+       
+
+        JLabel currentStatus = new JLabel("Current Status: " + status, SwingConstants.CENTER);
+
+
+
     
        
 
         tables.add(button);
+        tables.add(availability);
+        tables.add(choose);
+        tables.add(submit);
+        tables.add(currentStatus);
        
 
         button.addActionListener(new ActionListener(){
@@ -28,7 +55,24 @@ public class TableView extends JPanel {
             }
             
         });
-            
+
+        submit.addActionListener(new ActionListener(){
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String s = (String) choose.getSelectedItem();
+                
+                if(s == "Available"){
+                    status = "Available";
+                }
+                else{
+                    status = "Occupied";
+                }
+                
+                
+            }
+
+        });
         
 
         return tables;

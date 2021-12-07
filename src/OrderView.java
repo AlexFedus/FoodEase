99ca6ceb5dad82378e4644a614af7a2be581dd96
Evan Johnson <71622014+EvanJohnson64@users.noreply.github.com>
2021-12-07 18:15:09 -0500

    Finished OrderView and Order

--- src/OrderView.java
+++ src/OrderView.java
@@ -1,32 +1,64 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
 public class OrderView extends JPanel {
-	
-	public static JPanel newJPanel() {
-		JPanel orders = new JPanel();
+    static String[] orderInfo = new String[5];
+
+    public static JPanel newJPanel() {
+        JPanel orders = new JPanel();
         orders.setLayout(new FlowLayout());
 
-        JLabel text1 = new JLabel("Enter Table Number:");
-        JTextField tableNum = new JTextField();
+        JButton back = new JButton("Back");
+        JLabel text = new JLabel("Enter Table Number:");
+        JTextField tableNum = new JTextField(30);
 
-        JLabel text2 = new JLabel("Enter Meals:");
-
-        JTextField Meal1 = new JTextField("Meal 1:");
-        JTextField Meal2 = new JTextField("Meal 2:");
-        JTextField Meal3 = new JTextField("Meal 3:");
-        JTextField Meal4 = new JTextField("Meal 4:");
-        JTextField Meal5 = new JTextField("Meal 5:");
-
-        orders.add(text1);
+        JLabel text1 = new JLabel("Meal 1:");
+        JTextField Meal1 = new JTextField(40);
+        JLabel text2 = new JLabel("Meal 2:");
+        JTextField Meal2 = new JTextField(40);
+        JLabel text3 = new JLabel("Meal 3:");
+        JTextField Meal3 = new JTextField(40);
+        JLabel text4 = new JLabel("Meal 4:");
+        JTextField Meal4 = new JTextField(40);
+        JLabel text5 = new JLabel("Meal 5:");
+        JTextField Meal5 = new JTextField(40);
+        JButton submit = new JButton("Finish");
+        orders.add(text);
         orders.add(tableNum);
-        orders.add(text2);
+        orders.add(text1);
         orders.add(Meal1);
+        orders.add(text2);
         orders.add(Meal2);
+        orders.add(text3);
         orders.add(Meal3);
+        orders.add(text4);
         orders.add(Meal4);
+        orders.add(text5);
         orders.add(Meal5);
-        
-		return orders;
-	}
+        orders.add(submit);
+        orders.add(back);
+
+        submit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                orderInfo[0] = (tableNum.getText());
+                orderInfo[1] = (Meal1.getText());
+                orderInfo[2] = (Meal2.getText());
+                orderInfo[3] = (Meal3.getText());
+                orderInfo[4] = (Meal4.getText());
+                orderInfo[5] = (Meal5.getText());
+            }
+        });
+        back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPanel back = Orders.newJPanel();
+                Main.frame.setContentPane(back);
+                Main.frame.invalidate();
+                Main.frame.validate();
+            }
+        });
+        return orders;
+    }
 }
--- src/Orders.java
+++ src/Orders.java
@@ -1,45 +1,59 @@
 import java.awt.event.*;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.awt.*;
+import java.util.*;
 import javax.swing.*;
 
-
-
 public class Orders extends JPanel {
-	JTextField[] Orders = new JTextField[15];
-	Queue<JTextField> FoodQueue = new PriorityQueue<JTextField>();
-	
+    public static Queue<String[]> FoodQueue = new PriorityQueue<String[]>();
+
     public static JPanel newJPanel() {
         JPanel orders = new JPanel();
+        orders.setLayout(new FlowLayout(FlowLayout.CENTER));
+
         JButton button = new JButton("Home");
         JButton button1 = new JButton("New Order");
+        JButton button2 = new JButton("Edit Order");
+
         orders.add(button);
         orders.add(button1);
+        orders.add(button2);
 
-        button.addActionListener(new ActionListener(){
+        button.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
-            JPanel home = HomePage.newJPanel();
-            Main.frame.setContentPane(home);
-            Main.frame.invalidate();
-            Main.frame.validate();
-                
+                JPanel home = HomePage.newJPanel();
+                Main.frame.setContentPane(home);
+                Main.frame.invalidate();
+                Main.frame.validate();
+
             }
-            
+
         });
-        
+
         button1.addActionListener(new ActionListener() {
 
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				JPanel orderView = OrderView.newJPanel();
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPanel orderView = OrderView.newJPanel();
                 Main.frame.setContentPane(orderView);
+                Main.frame.invalidate();
                 Main.frame.validate();
-			}
-        	
+                FoodQueue.add(OrderView.orderInfo);
+            }
         });
-            
+
+        button2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JPanel orderView = OrderView.newJPanel();
+                Main.frame.setContentPane(orderView);
+                Main.frame.invalidate();
+                Main.frame.validate();
+            }
+        });
+
         return orders;
     }
 }
